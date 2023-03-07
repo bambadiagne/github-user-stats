7607cd39dc32ca455c0e679071ba149d804b126e
bambadiagne <diagne.bambahmadou@gmail.com>
2023-03-07 20:43:31 +0000

    Fix errors on requirements.txt and add some stuffs

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request
 from query import *
 from flask_apscheduler import APScheduler
 from flask_cors import CORS
-
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -13,15 +13,14 @@ scheduler.init_app(app)
 scheduler.start()
 
 
-
 @scheduler.task('interval', id='do_fetch_senegal_users',
                 seconds=3600, misfire_grace_time=900)
 def get_dakar_users():
-    start_time=time.time()
+    start_time = time.time()
     print('============================= START JOB =============================')
     all_users = {'users': []}
 
-    all_users['users']=user_fetcher() 
+    all_users['users'] = user_fetcher()
     with open("users.json", "w", encoding="utf-8",) as f:
         json.dump(all_users,
                   f,
@@ -29,37 +28,60 @@ def get_dakar_users():
                   sort_keys=True)
     print('============================= END JOB =============================')
     print(f"JOB TAKEN TIME {time.time()-start_time} seconds")
-    
+
+
 @app.route('/users/contributions/senegal', methods=['GET'])
 def fetch_senegal_users():
-    json_file =open('users.json','r')
+    json_file = open('users.json', 'r')
     data = json.loads(json_file.read())
     return jsonify(data)
-    
 
 
 @app.route('/users/search', methods=['GET'])
 def list_users_by_location():
     query_args = request.args
     variables = {'query': query_builder_string(query_args)}
-    data = user_fetcher(query_list_user(
+    users = []
+    data = handle_response(
+        user_fetcher(
+            query_list_user(
                 query_builder_string(query_args),
-                query_args.get('after')),variables=variables,single_fetch=True)
-    
-    
-    return jsonify(handle_response(data,"search"))
+                query_args.get('after')),
+            variables=variables,
+            single_fetch=True),
+        "search")
+    if (data.get('message')):
+        return jsonify(data)
+    cursor = data['pageInfo']['endCursor']
+    users.extend(data['nodes'])
+    while (data["pageInfo"]['hasNextPage']):
+        data = handle_response(user_fetcher(query_list_user(
+            query_builder_string(query_args),
+            cursor), variables=variables, single_fetch=True), "search")
+        if (data.get('message')):
+            return jsonify(data)
+        cursor = data["pageInfo"]['endCursor']
+        users.extend(data['nodes'])
+    return jsonify(users)
 
 
 @app.route('/users/<username>', methods=['GET'])
 def get_user_by_user(username):
-    
-    data=handle_response(user_fetcher(query_get_one_user(username),single_fetch=True),"user")
-    if(data.get("message")):
+
+    data = handle_response(
+        user_fetcher(
+            query_get_one_user(username),
+            single_fetch=True),
+        "user")
+    if (data.get("message")):
         return jsonify(data)
     return jsonify(format_user(data))
+
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return {"message":"Ressource introuvable"}
+    return {"message": "Ressource introuvable"}
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host="0.0.0.0")
-    
+    app.run(debug=os.getenv('FLASK_DEBUG'), port=5000, host="0.0.0.0")
--- query.py
+++ query.py
@@ -5,7 +5,7 @@ import math
 
 DEFAULT_USERNAME = "torvalds"
 BASE_URL = "https://api.github.com/graphql"
-MAX_GITHUB_FETCH=1000
+MAX_GITHUB_FETCH = 1000
 
 GRAPHQL_SERVEUR = GraphqlClient(
     endpoint=BASE_URL, headers={
@@ -30,8 +30,8 @@ LOCATIONS_FOR_SENEGAL = " ".join(
 
 def query_builder_string(query: dict,):
     query = query.copy()
-    grapqhql_query = ""
-    
+    grapqhql_query = "type:user "
+
     # if (query.get('bio')):
     #     grapqhql_query += f"{query['bio']} in:bio"
     if (query.get('user')):
@@ -42,7 +42,7 @@ def query_builder_string(query: dict,):
             grapqhql_query += f" location:{location}"
     if (query.get("language")):
         for language in query.getlist("language"):
-            grapqhql_query += f" language:{language}"     
+            grapqhql_query += f" language:{language}"
     return grapqhql_query
 
 
@@ -66,7 +66,7 @@ def query_list_user(location='Senegal', after=''):
 }}""".format(location, "after: \"{}\"".format(after) if (after) else '')
 
 
-def query_all_senegalese(query,number, after=''):
+def query_all_senegalese(query, number, after=''):
     return """query GithubSenegaleseUser($query: String = "{0}") {{
   search(query: $query, type: USER, first:{1}, {2}) {{
     userCount
@@ -74,6 +74,7 @@ def query_all_senegalese(query,number, after=''):
       ... on User {{
         name
         login
+        avatarUrl
         contributionsCollection {{
           totalCommitContributions
           contributionCalendar {{
@@ -111,7 +112,7 @@ def query_get_one_user(login=DEFAULT_USERNAME):
       first: 100
       isFork: false
       ownerAffiliations: OWNER
-   
+
     ) {{
       edges {{
         node {{
@@ -136,52 +137,64 @@ def query_get_one_user(login=DEFAULT_USERNAME):
 
 
 def fetchAllSenegalese(query):
-    all_users=[] 
+    all_users = []
     try:
-      data = handle_response(asyncio.run(
-        GRAPHQL_SERVEUR.execute_async(
-            query=query_all_senegalese(query,50),
-        )),"search")
+        data = handle_response(asyncio.run(
+            GRAPHQL_SERVEUR.execute_async(
+                query=query_all_senegalese(query, 50),
+            )), "search")
     except RuntimeError as e:
-        pass     
-    if(data.get('message')):
-      return {'users':None,'message':data.get('message')}  
-    userCount=data['userCount']
-    all_users.extend(user for user in data['nodes'] if(user!={}))#remove empty json
+        pass
+    if (data.get('message')):
+        return {'users': None, 'message': data.get('message')}
+    userCount = data['userCount']
+    all_users.extend(
+        user for user in data['nodes'] if (
+            user != {}))  # remove empty json
     while (data['pageInfo']['hasNextPage']):
         cursor = data['pageInfo']['endCursor']
         try:
-  
-          data = handle_response(asyncio.run(
-            GRAPHQL_SERVEUR.execute_async(
-                query=query_all_senegalese(query,50,cursor),
-            )),"search")
-          if(data.get('message')):
-            return {'users':None,'message':data.get('message')}
+
+            data = handle_response(asyncio.run(
+                GRAPHQL_SERVEUR.execute_async(
+                    query=query_all_senegalese(query, 50, cursor),
+                )), "search")
+            if (data.get('message')):
+                return {'users': None, 'message': data.get('message')}
         except RuntimeError as e:
-          pass   
-        all_users.extend(user for user in data['nodes'] if(user!={}))#
-    return {'users':all_users,'userCount':userCount,'message':"SUCCESSFUL"}    
-def user_fetcher(query=None,variables=None,single_fetch=False):
-    
-    if(single_fetch):
-      data = asyncio.run(
-        GRAPHQL_SERVEUR.execute_async(query=query,
-                                      variables=variables
-        ))
-      return data
-    fetched_result=fetchAllSenegalese(LOCATIONS_FOR_SENEGAL+" sort:joined")
-    last_joined_user=fetched_result['users'][-1]
-    all_users=list(fetched_result['users']) 
-    remaining_requests=math.ceil(fetched_result['userCount']/MAX_GITHUB_FETCH)
+            pass
+        all_users.extend(user for user in data['nodes'] if (user != {}))
+    return {
+        'users': all_users,
+        'userCount': userCount,
+        'message': "SUCCESSFUL"}
+
+
+def user_fetcher(query=None, variables=None, single_fetch=False):
+
+    if (single_fetch):
+        data = asyncio.run(
+            GRAPHQL_SERVEUR.execute_async(query=query,
+                                          variables=variables
+                                          ))
+        return data
+    fetched_result = fetchAllSenegalese(LOCATIONS_FOR_SENEGAL + " sort:joined")
+    last_joined_user = fetched_result['users'][-1]
+    all_users = list(fetched_result['users'])
+    remaining_requests = math.ceil(
+        fetched_result['userCount'] /
+        MAX_GITHUB_FETCH)
     for i in range(remaining_requests):
-      users=fetchAllSenegalese(LOCATIONS_FOR_SENEGAL+f" sort:joined created:<{last_joined_user['createdAt']}")
-      all_users.extend(users['users'])    
-      if(users['users']==[]):
-          break
-      last_joined_user=users['users'][-1]
-  
+        users = fetchAllSenegalese(
+            LOCATIONS_FOR_SENEGAL +
+            f" sort:joined created:<{last_joined_user['createdAt']}")
+        all_users.extend(users['users'])
+        if (users['users'] == []):
+            break
+        last_joined_user = users['users'][-1]
+
     return all_users
+
 
 def count_user_in_json(json_array, login):
     count = 0
@@ -192,28 +205,33 @@ def count_user_in_json(json_array, login):
             count += 1
     return count
 
+
 def format_user(user):
-  languages_dict={}
-  for node in user['repositories']['edges']:
-      for language in node['node']['languages']['edges']:
-        language_type=language['node']['name']
-        if(languages_dict.get(language_type)):
-          languages_dict[language_type]+=language['size']
-        else:    
-          languages_dict[language_type]=language['size']
-  user['most_used_anguages']=list(map(lambda x:x[0],sorted(languages_dict.items(), key=lambda item: item[1],reverse=True)[:5])) 
-  user['most_starred_repo']=user['repositories']['edges'][0]['node'] if(user['repositories']['edges']) else None
-  del user['most_starred_repo']['languages']
-  del user['repositories']  
-  return user
+    languages_dict = {}
+    for node in user['repositories']['edges']:
+        for language in node['node']['languages']['edges']:
+            language_type = language['node']['name']
+            if (languages_dict.get(language_type)):
+                languages_dict[language_type] += language['size']
+            else:
+                languages_dict[language_type] = language['size']
+    user['most_used_anguages'] = list(map(lambda x: x[0], sorted(
+        languages_dict.items(), key=lambda item: item[1], reverse=True)[:5]))
+    user['most_starred_repo'] = user['repositories']['edges'][0]['node'] if (
+        user['repositories']['edges']) else None
+    del user['most_starred_repo']['languages']
+    del user['repositories']
+    return user
 
 
-def handle_response(response:dict,data_type):
-    if(response.get('message')):
-        return {'message':response.get('message')}
-    if(response.get('errors')):
-       return {'message':response['errors'][-1]['message']}
+def handle_response(response: dict, data_type):
+    if (response.get('message')):
+        return {'message': response.get('message')}
+    if (response.get('errors')):
+        return {'message': response['errors'][-1]['message']}
     return response['data'][data_type]
+
+
 def intersection(arr1, arr2):
     users = []
     if (len(arr1) > len(arr2)):
